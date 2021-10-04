42dc5b021fa2fae47e32580af2e702359f2f84f3
02rasjac <rasmusjacklin2002@gmail.com>
2021-10-04 15:36:13 +0200

    Refactor and cleaning

--- Solsystem.m
+++ Solsystem.m
@@ -2,12 +2,14 @@
     p  = Cell of planets names
     r  = The planets orbital radius
     t  = The planets orbital timeperiod
+    pr = The planets radius
     pm = Mass of the planets
-    mp = Månarnas planet de rör sig runt
-    mr = Månbanornas radie runt planeten
-    mt = Månarnas omloppstid
+    m  = The names of the moons
+    mp = The planets the moon orbits
+    mr = The moons orbital radius
+    mt = The moons orbital timeperiod
 %}
-function film = Solsystem(p, r, t, pr)
+function film = Solsystem(p, r, t, pr, m, mp, mr, mt)
     earthDay = 0;
     nDays = 365;
 
@@ -19,21 +21,52 @@ function film = Solsystem(p, r, t, pr)
         hold on
         for planet = p
             pName = planet{1};
-            dis = sqrt(r(pName));
-            [x, y] = GetCircle(dis, t(pName), [0, 0]);
-            plot(x, y, "--");
+            
+            % Planets orbit
+            [x, y] = DrawOrbit(r(pName), t(pName));
 
-            xOff = x(mod(earthDay, floor(t(pName)))+1);
-            yOff = y(mod(earthDay, floor(t(pName)))+1);
-            pRadius = sqrt(pr(pName) / pi) * 15;
-            [px, py] = GetCircle(pRadius, 100, [xOff, yOff]);
-            fill(px, py, "r");
+            % Planet itself
+            [px, py] = PlanetCoord(earthDay, t(pName), x, y);
+            DrawBody(pr(pName), px, py);
+
         end
         hold off
         axis equal
         earthDay = earthDay + 1;
         film(earthDay) = getframe(gcf);
     end
+end
+
+%{
+    Returns x and y coords for the edge of the orbit
+    radius = Radius, obviously
+    period = number of days it takes for the planet
+%}
+function [x, y] = DrawOrbit(radius, period)
+    dis = sqrt(radius);
+    [x, y] = GetCircle(dis, period, [0, 0]);
+    plot(x, y, "--");
+end
+
+%{
+    radius = Radius, obviously
+    x, y = The coordinates of the planet, positioned on their orbit
+%}
+function DrawBody(radius, x, y)
+    radius = sqrt(radius / pi) * 15;
+    [px, py] = GetCircle(radius, 100, [x, y]);
+    fill(px, py, "r");
+end
+
+%{
+    Returns x and y coordinate for a planet after days number of earthdays
+    days = earthdays since start
+    pPeriod = Number of days the planet takes to orbit 360degrees
+    ox, oy = The coordinates of the orbits
+%}
+function [xOff, yOff] = PlanetCoord(days, pPeriod, ox, oy)
+    xOff = ox(mod(days, floor(pPeriod))+1);
+    yOff = oy(mod(days, floor(pPeriod))+1);
 end
 
 %{
--- data.m
+++ data.m
@@ -15,13 +15,15 @@ P_OrbTimes = containers.Map(planets, orbTime);
 clear masses radius orbRadius orbTime;
 
 %* månar
-moons = {'moon','phobos','ganymedes','titan','titania','triton','charon'};
-masses = [0.07342*10^24, 1.07*10^16, 1.4819*10^23, 1.3452*10^23, 3.527*10^21, 2.147*10^22, 1.52*10^21]; %(kg)
-radius = [1737.1, 11.1, 2634.1, 2576, 788.4, 1353.4, 606]; %(km)
-orbRadius = [384400, 9376, 1070400, 1221870, 435910, 354800, 19571]; %(km)
-orbTime = [27.3216, 0.3189, 7.1545, 15.945, 8.7062, 5.877, 6.3872]; %(dygn)
+moons     = {'moon',       'phobos',   'ganymedes',  'titan',      'titania',   'triton',    'charon'};
+orbPlan   = {'earth',      'mars',     'jupiter',    'saturn',     'uranus',    'neptune',   'pluto'};
+masses    = [0.07342*10^24, 1.07*10^16, 1.4819*10^23, 1.3452*10^23, 3.527*10^21, 2.147*10^22, 1.52*10^21]; %(kg)
+radius    = [1737.1,        11.1,       2634.1,       2576,         788.4,       1353.4,      606]; %(km)
+orbRadius = [384400,        9376,       1070400,      1221870,      435910,      354800,      19571]; %(km)
+orbTime   = [27.3216,       0.3189,     7.1545,       15.945,       8.7062,      5.877,       6.3872]; %(dygn)
 
 %* Maps för månar
+M_OrbPlan = containers.Map(moons, orbPlan);
 M_Masses = containers.Map(moons,masses);
 M_Radius = containers.Map(moons,radius);
 M_OrbRadius = containers.Map(moons,orbRadius);
